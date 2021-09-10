3c2546052d1f7623f0852d73e5da25740a0de9d6
ParasJain-knoluds <paras.jain@knoldus.com>
2021-09-10 19:53:14 +0530

    adding project files

---
+++ src/OpenClose/User.java
@@ -0,0 +1,14 @@
+package OpenClose;
+
+public class User {
+    int userId;
+    String name;
+    String address;
+    int contactNumber;
+    User(){
+        userId=1375;
+        name="Paras Jain";
+        address="Delhi";
+        contactNumber= 987036534;
+    }
+}
